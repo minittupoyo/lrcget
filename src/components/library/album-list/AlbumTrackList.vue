7ae62a1ae9c50512cf955a041a93c9c9558d5fa1
minittu <minittu.4989@gmail.com>
2026-08-29 16:06:54 +0900

    feat: localize album track list

--- src/components/library/album-list/AlbumTrackList.vue
+++ src/components/library/album-list/AlbumTrackList.vue
@@ -18,7 +18,7 @@
             {{ album.name }}
           </div>
           <div class="flex items-center gap-2">
-            <div class="text-sm text-brave-30 group-hover:text-brave-20 transition dark:text-white">{{ album.tracks_count }} tracks</div>
+            <div class="text-sm text-brave-30 group-hover:text-brave-20 transition dark:text-white">{{ $t('library.trackCount', { count: album.tracks_count }) }}</div>
             <div class="border-r border-brave-80 h-3 flex-none"></div>
             <div class="text-sm text-brave-30 group-hover:text-brave-20 transition dark:text-white">{{ album.artist_name }}</div>
           </div>
@@ -28,7 +28,7 @@
           <button class="button button-normal px-4 py-1.5 text-xs rounded-full" @click.prevent="downloadAlbumLyrics">
           <div class="text-sm"><DownloadMultiple /></div>
           <span>
-            Download album lyrics
+            {{ $t('library.downloadAlbumLyrics') }}
           </span>
         </button>
         </div>
@@ -38,9 +38,9 @@
         <div class="w-full flex">
           <div class="text-xs text-brave-30/70 font-bold flex w-full dark:text-brave-95">
             <div class="text-right flex-none w-[5%] p-1 pr-2">#</div>
-            <div class="text-left flex-none w-[60%] p-1">Track</div> <!-- Adjusted width percentage -->
-            <div class="text-right flex-none w-[10%] p-1">Duration</div>
-            <div class="text-center flex-none w-[10%] p-1">Lyrics</div>
+            <div class="text-left flex-none w-[60%] p-1">{{ $t('library.track') }}</div> <!-- Adjusted width percentage -->
+            <div class="text-right flex-none w-[10%] p-1">{{ $t('library.duration') }}</div>
+            <div class="text-center flex-none w-[10%] p-1">{{ $t('library.lyrics') }}</div>
             <div class="text-right flex-none w-[15%] p-1"></div>
           </div>
         </div>
