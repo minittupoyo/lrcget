a14cafb6da57524f0e0f57329c268ecf1ca4a715
minittu <minittu.4989@gmail.com>
2026-08-29 15:43:49 +0900

    feat: localize configuration modal

--- src/components/library/Config.vue
+++ src/components/library/Config.vue
@@ -1,124 +1,61 @@
 <template>
   <BaseModal
-    title="Configuration"
+    :title="$t('config.title')"
     @beforeOpen="beforeOpenHandler"
     @close="emit('close')"
     body-class="flex flex-col h-full justify-between overflow-y-auto"
   >
     <div class="flex flex-col gap-8">
       <div>
-        <label class="group-label mb-4">Common</label>
+        <label class="group-label mb-4">{{ $t('config.common') }}</label>
 
         <div class="flex flex-col mb-4">
-          <label class="block mb-2 child-label">Download lyrics for</label>
-
-          <RadioButton
-              class="mb-1"
-              v-model="downloadLyricsFor"
-              name="download-lyrics-for"
-              id="download-lyrics-for-all"
-              value="all"
-            >
-              Download lyrics for all songs
-            </RadioButton>
-
-            <RadioButton
-              class="mb-1"
-              v-model="downloadLyricsFor"
-              name="download-lyrics-for"
-              id="skip-synced"
-              value="skipSynced"
-            >
-              Download lyrics for songs without synced lyrics
-            </RadioButton>
-
-            <RadioButton
-              class="mb-1"
-              v-model="downloadLyricsFor"
-              name="download-lyrics-for"
-              id="skip-plain"
-              value="skipPlain"
-            >
-              Download lyrics for songs without plain or synced lyrics
-            </RadioButton>
+          <label class="block mb-2 child-label">{{ $t('config.downloadLyricsFor') }}</label>
+          <RadioButton class="mb-1" v-model="downloadLyricsFor" name="download-lyrics-for" id="download-lyrics-for-all" value="all">{{ $t('config.allSongs') }}</RadioButton>
+          <RadioButton class="mb-1" v-model="downloadLyricsFor" name="download-lyrics-for" id="skip-synced" value="skipSynced">{{ $t('config.withoutSynced') }}</RadioButton>
+          <RadioButton class="mb-1" v-model="downloadLyricsFor" name="download-lyrics-for" id="skip-plain" value="skipPlain">{{ $t('config.withoutPlainOrSynced') }}</RadioButton>
         </div>
 
         <div class="flex flex-col mb-4">
-          <label class="block mb-2 child-label">Search settings</label>
-
-          <CheckboxButton
-              v-model="showLineCount"
-              name="show-line-count"
-              id="show-line-count"
-            >
-              Show the number of lines a lyric file has in the search menu
-          </CheckboxButton>
+          <label class="block mb-2 child-label">{{ $t('config.searchSettings') }}</label>
+          <CheckboxButton v-model="showLineCount" name="show-line-count" id="show-line-count">{{ $t('config.showLineCount') }}</CheckboxButton>
         </div>
 
         <div class="flex flex-col mb-4">
-          <label class="block mb-2 child-label">Theme mode</label>
-
+          <label class="block mb-2 child-label">{{ $t('config.themeMode') }}</label>
           <div class="flex gap-4">
-            <RadioButton
-              v-model="editingThemeMode"
-              name="theme-mode"
-              id="theme-auto"
-              value="auto"
-            >
-              Auto
-            </RadioButton>
-
-            <RadioButton
-              v-model="editingThemeMode"
-              name="theme-mode"
-              id="theme-light"
-              value="light"
-            >
-              Light
-            </RadioButton>
-
-            <RadioButton
-              v-model="editingThemeMode"
-              name="theme-mode"
-              id="theme-dark"
-              value="dark"
-            >
-              Dark
-            </RadioButton>
+            <RadioButton v-model="editingThemeMode" name="theme-mode" id="theme-auto" value="auto">{{ $t('config.auto') }}</RadioButton>
+            <RadioButton v-model="editingThemeMode" name="theme-mode" id="theme-light" value="light">{{ $t('config.light') }}</RadioButton>
+            <RadioButton v-model="editingThemeMode" name="theme-mode" id="theme-dark" value="dark">{{ $t('config.dark') }}</RadioButton>
           </div>
         </div>
 
         <div class="flex flex-col">
-          <label class="block mb-2 child-label" for="lrclib-instance">LRCLIB instance</label>
+          <label class="block mb-2 child-label" for="lrclib-instance">{{ $t('config.lrclibInstance') }}</label>
           <input id="lrclib-instance" type="text" v-model="editingLrclibInstance" placeholder="https://" class="input px-4 h-8">
         </div>
       </div>
 
       <div>
-        <label class="group-label mb-4">Experimental</label>
-
+        <label class="group-label mb-4">{{ $t('config.experimental') }}</label>
         <div class="flex items-start">
-          <CheckboxButton
-            v-model="tryEmbedLyrics"
-            name="try-embed-lyrics"
-            id="try-embed-lyrics"
-          >
+          <CheckboxButton v-model="tryEmbedLyrics" name="try-embed-lyrics" id="try-embed-lyrics">
             <div class="flex flex-col">
-              <span class="mb-0.5">Try to embed the lyrics to the track files when possible</span>
-              <span class="text-xs text-yellow-700 dark:text-yellow-400">This option could corrupt your track files. Make sure to backup your library before enabling it.</span>
+              <span class="mb-0.5">{{ $t('config.embedLyrics') }}</span>
+              <span class="text-xs text-yellow-700 dark:text-yellow-400">{{ $t('config.embedWarning') }}</span>
             </div>
           </CheckboxButton>
         </div>
       </div>
 
       <div class="flex flex-col gap-1">
-        <a href="#" class="link" @click="refreshLibrary">Refresh my library for new changes...</a>
-        <a href="#" class="link" @click="uninitializeLibrary">Add and remove scanning directories...</a>
+        <a href="#" class="link" @click="refreshLibrary">{{ $t('config.refreshLibrary') }}</a>
+        <a href="#" class="link" @click="uninitializeLibrary">{{ $t('config.manageDirectories') }}</a>
       </div>
     </div>
 
     <template #footer>
-      <button class="button button-primary px-8 py-2 rounded-full" @click="save">Save</button>
+      <button class="button button-primary px-8 py-2 rounded-full" @click="save">{{ $t('config.save') }}</button>
     </template>
   </BaseModal>
 </template>
@@ -131,9 +68,7 @@ import RadioButton from '@/components/common/RadioButton.vue'
 import CheckboxButton from '@/components/common/CheckboxButton.vue'
 
 const { setThemeMode, setLrclibInstance } = useGlobalState()
-
 const emit = defineEmits(['close', 'refreshLibrary', 'uninitializeLibrary'])
-
 const downloadLyricsFor = ref('all')
 const skipTracksWithSyncedLyrics = ref(true)
 const skipTracksWithPlainLyrics = ref(false)
@@ -156,31 +91,16 @@ const save = async () => {
   emit('close')
 }
 
-const refreshLibrary = () => {
-  emit('refreshLibrary')
-  emit('close')
-}
-
-const uninitializeLibrary = () => {
-  emit('uninitializeLibrary')
-  emit('close')
-}
+const refreshLibrary = () => { emit('refreshLibrary'); emit('close') }
+const uninitializeLibrary = () => { emit('uninitializeLibrary'); emit('close') }
 
 const beforeOpenHandler = async () => {
   const config = await invoke('get_config')
   skipTracksWithSyncedLyrics.value = config.skip_tracks_with_synced_lyrics
   skipTracksWithPlainLyrics.value = config.skip_tracks_with_plain_lyrics
-
-  console.log(skipTracksWithSyncedLyrics.value, skipTracksWithPlainLyrics.value)
-
-  if (skipTracksWithSyncedLyrics.value && !skipTracksWithPlainLyrics.value) {
-    downloadLyricsFor.value = 'skipSynced'
-  } else if (skipTracksWithPlainLyrics.value) {
-    downloadLyricsFor.value = 'skipPlain'
-  } else {
-    downloadLyricsFor.value = 'all'
-  }
-
+  if (skipTracksWithSyncedLyrics.value && !skipTracksWithPlainLyrics.value) downloadLyricsFor.value = 'skipSynced'
+  else if (skipTracksWithPlainLyrics.value) downloadLyricsFor.value = 'skipPlain'
+  else downloadLyricsFor.value = 'all'
   showLineCount.value = config.show_line_count
   tryEmbedLyrics.value = config.try_embed_lyrics
   editingThemeMode.value = config.theme_mode
